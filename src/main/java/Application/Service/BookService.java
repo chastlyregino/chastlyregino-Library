9be7dadc2771dd66a62ee5963b3f542c4a1f3425
Finn Chastly Manapat <chastly_regino@icloud.com>
2024-07-16 18:22:06 +0000

    creating SQL statements for DAOs and using them in Service.java

--- src/main/java/Application/Service/BookService.java
+++ src/main/java/Application/Service/BookService.java
@@ -40,7 +40,7 @@ public class BookService {
      * @return all books.
      */
     public List<Book> getAllBooks() {
-        return null;
+        return this.bookDAO.getAllBooks();
     }
     /**
      * TODO: Use the bookDAO to persist a book to the database.
@@ -51,7 +51,12 @@ public class BookService {
      * key was already in use.)
      */
     public Book addBook(Book book) {
+        Book isbnInUse = this.bookDAO.getBookByIsbn(book.getIsbn());
 
+        if (isbnInUse == null) {
+            return this.bookDAO.insertBook(book);
+        }
+        
         return null;
     }
     /**
@@ -59,7 +64,7 @@ public class BookService {
      * @return all available books (bookCount over zero)
      */
     public List<Book> getAllAvailableBooks() {
-        return null;
+        return this.bookDAO.getBooksWithBookCountOverZero();
     }
 
 }
